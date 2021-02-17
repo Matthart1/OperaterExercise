db03562894a10e0d7fa9b3bab6888aa8aa43ca2d
Matt Hart <mp.hart047@gamil.com>
2021-02-16 20:17:35 -0600

    the rest of it

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+
+            Console.WriteLine($"{a}/{b} is {div} with a remaineder of {mod}");        }      
+    }
+}
